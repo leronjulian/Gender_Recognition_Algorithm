6a05b5278d4b69783d6a1ef2174c07ab18179175
Leron Julian <leronjulian@hotmail.com>
2018-10-21 00:37:41 -0400

    updated algorithm and added env

--- Gender_Rec.py
+++ Gender_Rec.py
@@ -99,21 +99,21 @@ def facecrop(image):
 
 	img = cv2.imread(image)
 
+	print(img)
 	minisize = (img.shape[1],img.shape[0])
 	miniframe = cv2.resize(img, minisize)
-
+	
 	faces = cascade.detectMultiScale(miniframe)
-
+	
 	for f in faces:
 		x, y, w, h = [ v for v in f ]
-		cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255))
-
+		cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255)) 
 		sub_face = img[y:y+h, x:x+w]
 		fname, ext = os.path.splitext(image)
 		cv2.imwrite(fname+"_cropped_"+ext, sub_face)
 
-
 	original_file_name = image.split('.')[0]
+
 	return(original_file_name + "_cropped_.jpg")
 # ========================== End image cropping =============================
 
